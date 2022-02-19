331efc098dd043a4f5295b791efa6ddc599c835c
alejuran <alejandrourn@gmail.com>
2022-02-19 11:47:29 -0500

    diagonal commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,18 +12,19 @@ void print_diagonal(int n)
 	int x, y;
 
 	if (n > 0)
+	{
 
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+
+				_putchar(' ');
+
+			_putchar('\\');
+
+			_putchar('n');
+		}
+	}
+	else
 		_putchar('\n');
-
-			for (x = 0; x < n; x++)
-			{
-				for (y = 0; y < x; y++)
-				{
-					_putchar(' ');
-				}
-
-				_putchar('\\');
-
-					_putchar('n');
-			}
 }
