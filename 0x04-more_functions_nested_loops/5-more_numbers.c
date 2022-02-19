6a688c18a63e93b8cd5bf065be10ae15ecd9daf8
alejuran <alejandrourn@gmail.com>
2022-02-19 08:23:41 -0500

    more commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,18 @@ void more_numbers(void)
 	int x;
 	int y;
 
-		for (y = 0; y <= 10; y++)
+	for (y = 0; y <= 10; y++)
 
+	{
+		for (x = 0; x <= 14; x++)
 		{
-			for (x = 0; x <= 14; x++)
+			if (x > 9)
 
-				_putchar(x);
-
-			_putchar('\n');
-
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
 		}
+
+		_putchar('\n');
+
+	}
 }
