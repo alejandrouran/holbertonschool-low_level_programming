44ad2d84a17d82ed3fb51e61152a8c8545b518d1
alejuran <alejandrourn@gmail.com>
2022-02-21 17:20:03 -0500

    rev commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: print string in reverse
+ *
+ * Return: no value
+ */
+void print_rev(char *s)
+{
+	int p = 0;
+
+	while (*(s + p) != '\0')
+	{
+		p++;
+	}
+
+	while (p >= 0)
+	{
+		_putchar(*(s + p));
+		p--;
+
+	}
+	_putchar('\n');
+}
