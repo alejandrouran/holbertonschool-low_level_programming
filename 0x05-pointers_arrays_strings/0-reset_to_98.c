c07c9f36afd28123d70bda112828afb3335f1638
alejuran <alejandrourn@gmail.com>
2022-02-21 16:36:39 -0500

    reset commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98.c - as parameter and updates the value it point to 98
+ * reset_to_98 - as parameter and updates the value it point to 98
  * @n: punter to n
  *
  * Return: no value
