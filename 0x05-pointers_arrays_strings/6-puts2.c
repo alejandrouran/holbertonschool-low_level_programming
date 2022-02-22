502e4c02d84c55bd8191d223932395383bac880b
alejuran <alejandrourn@gmail.com>
2022-02-21 20:33:20 -0500

    puts2 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: character
+ *
+ * Return: no value
+ */
+void puts2(char *str)
+{
+	int p = 0;
+
+	while (*(str + p) != '\0')
+	{
+		p = p + 2;
+			_putchar(*(str + p));
+		p++;
+	}
+	_putchar('\n');
+}
