057dbf462e17acac31fb48801ed32dd36905ffe6
alejuran <alejandrourn@gmail.com>
2022-02-24 18:31:24 -0500

    second commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: copies
+ * @src: copies
+ * @n: copies
+ *
+ * Return: to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
