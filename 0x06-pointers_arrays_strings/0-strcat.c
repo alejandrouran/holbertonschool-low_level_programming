bcff7a1ca362470c73284aa5f2e5e90d79cf698d
alejuran <alejandrourn@gmail.com>
2022-02-24 10:16:20 -0500

    strc commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,9 +14,9 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; dest[i] != '\0'; ++i)
+	for (i = 0; dest[i] != '\0'; i++)
 
-		for (j = 0; src[i] != '\0'; ++j, ++i)
+		for (j = 0; src[i] != '\0'; j++, i++)
 		{
 			dest[i] = src[j];
 		}
