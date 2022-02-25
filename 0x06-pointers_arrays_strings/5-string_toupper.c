2b8d4d068df0851e6688821b4da8b011fed51d7e
alejuran <alejandrourn@gmail.com>
2022-02-25 17:16:39 -0500

    toupper commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string.
+ * @str: string convert
+ *
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+	int desp = 'a' - 'A';
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (str[i] >= 'a' && str[i] <= 'Z')
+		{
+			str[i] = str[i] - desp;
+		}
+	}
+	return (str);
+}
