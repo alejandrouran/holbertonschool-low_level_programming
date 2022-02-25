259a26aab1604a85283538912e36a5b7737f31ef
alejuran <alejandrourn@gmail.com>
2022-02-24 21:22:01 -0500

    third commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: 0
+ * Return: *s1 and *s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -15,5 +15,5 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 
-	return (0);
+	return (*s1 - *s2);
 }
