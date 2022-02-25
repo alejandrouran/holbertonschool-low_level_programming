b475802ba63d71ddb50590b7721e2e6e39af2403
alejuran <alejandrourn@gmail.com>
2022-02-25 17:45:48 -0500

    toupper commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,12 @@
 char *string_toupper(char *str)
 {
 	int i = 0;
-	int desp = 'a' - 'A';
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'Z')
 		{
-			str[i] = str[i] - desp;
+			str[i] -= 32;
 		}
 	}
 	return (str);
