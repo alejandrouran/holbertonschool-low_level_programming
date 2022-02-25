919ac1b8c5457b549c6d783625403d033649936c
alejuran <alejandrourn@gmail.com>
2022-02-24 20:15:14 -0500

    second commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,20 +10,20 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0;
-	int j = 0;
+	int a = 0, j = 0;
+
 
 	while (src[a])
 		j++;
 
 	for (a = 0; a < n && src[a] != '\0'; a++)
-	{
+
 		dest[a] = src[a];
-	}
+
 	for (a = j; a < n; a++)
-	{
+
 		dest[a] = '\0';
-	}
+
 
 	return (dest);
 }
