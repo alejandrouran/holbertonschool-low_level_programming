f2bc04d1609b743be47be7c3419d0e1679da7a0e
alejuran <alejandrourn@gmail.com>
2022-02-26 19:17:20 -0500

    leet commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+# include "main.h"
+
+/**
+ * leet - encodes a string into
+ * @a: encode
+ *
+ * Return: string
+ */
+char *leet(char *a)
+{
+	int i;
+	int j;
+	int f[] = {'a', 'A', 'e', 'E', 'o', '0', 't', 'T', 'l', 'L'};
+	int r[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[i] == f[j])
+			{
+				a[i] = r[j / 2];
+				j = 9;
+			}
+		}
+	}
+	return (a);
+}
