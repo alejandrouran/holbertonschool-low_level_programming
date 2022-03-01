adb8ca9f0eaf01f935615729273fd7a1694f4e66
alejuran <alejandrourn@gmail.com>
2022-02-28 20:36:47 -0500

    str commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be checked
+ * @c: character to be checked
+ *
+ * Return: pointer to the firs ocurrence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+
+			return (s);
+
+		else
+			s++;
+	}
+	return (NULL);
+}
