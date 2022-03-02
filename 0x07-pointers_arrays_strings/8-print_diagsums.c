2fc79db8f965d4efe0fcf9c7410e68fbfc6c2cb1
alejuran <alejandrourn@gmail.com>
2022-03-01 21:35:09 -0500

    print commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * print_diagsums - the sum of the two diagonals
+ * @a: matrix of integers
+ * @size: size of matrix
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[(size + 1) * i];
+
+		s2 += a[(size - 1) * (i + 1)];
+	}
+	printf("%d, %d\n", s1, s2);
+}
