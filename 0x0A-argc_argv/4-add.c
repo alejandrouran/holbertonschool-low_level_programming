be935dbd5ed5d80c25bbad82711b38710615bc62
alejuran <alejandrourn@gmail.com>
2022-03-04 17:42:55 -0500

    add commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,11 +13,14 @@ int main(int argc, char **argv)
 	int i;
 	int s = 0;
 
+	if (argc < 1)
+		return (0);
+
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
 		{
-			printf("Error\n");
+			printf("%s\n", "Error");
 
 			return (1);
 		}
