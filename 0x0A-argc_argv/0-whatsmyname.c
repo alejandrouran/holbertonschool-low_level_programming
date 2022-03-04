9c926037fda975eff37820435cdfb9cc0c0ab29f
alejuran <alejandrourn@gmail.com>
2022-03-04 13:11:54 -0500

    name commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints is name
+ * @argc: number arguments
+ * @argv: array arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s. \n", argv[0]);
+
+	return (0);
+}
