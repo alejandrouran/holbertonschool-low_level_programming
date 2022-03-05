42c42e2751546a2be8fa2b0b3d6fe36a1a3368d8
alejuran <alejandrourn@gmail.com>
2022-03-04 19:26:24 -0500

    ad commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number arguments
+ * @argv: array arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int i, s = 0;
+
+	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+
+			return (1);
+		}
+		s += atoi(argv[i]);
+	}
+	printf("%d\n", s);
+
+	return (0);
+}
