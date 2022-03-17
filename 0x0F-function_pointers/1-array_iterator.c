b35e6fc4b40f78137ac103043087d94089772530
alejuran <alejandrourn@gmail.com>
2022-03-17 14:33:55 -0500

    array commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - function given as a parameter on each element of an array
+ * @array: pointer array
+ * @size: array
+ * @action: pointer
+ *
+ * Return: value
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+
+			action(array[i]);
+	}
+}
