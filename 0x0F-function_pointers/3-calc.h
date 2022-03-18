318386ad82b0c0418faf1ade5fab618e07cf2d97
alejuran <alejandrourn@gmail.com>
2022-03-17 20:34:15 -0500

    main commit

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,20 @@
-#ifndef 3_CALC_H
-#define 3_CALC_H
+#ifndef _3_CALC_H
+#define _3_CALC_H
 
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
-char *op;
-int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition function
+ * @a: add a
+ * @b: add b
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction function
+ * @a: sub a
+ * @b: sub b
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: mul a
+ * @b: mul b
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division function
+ * @a: div a
+ * @b: div b
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo function
+ * @a: mod a
+ * @b: mod a
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
