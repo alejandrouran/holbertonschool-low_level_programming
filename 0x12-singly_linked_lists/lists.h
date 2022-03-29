f08e9b7c9555a577f40b99958771bf0e215574f4
alejuran <alejandrourn@gmail.com>
2022-03-29 14:40:43 -0500

    lists commit

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length
+ * @next: points
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
