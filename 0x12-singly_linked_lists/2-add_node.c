82538d50b28c50c9dd323d4ed0fff299b9a691bf
alejuran <alejandrourn@gmail.com>
2022-03-29 16:23:07 -0500

    node commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - new node at the beginning
+ * @head: pointer
+ * @str: string
+ * Return: NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n;
+
+	n = malloc(sizeof(list_t));
+
+	if (n == NULL)
+
+		return (NULL);
+
+	n->str = strdup(str);
+	n->next = *head;
+	*head = n;
+
+	return (*head);
+}
