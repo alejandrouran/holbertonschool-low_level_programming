a1cfa06d9b69c7d63e8f7d691289c8811d3a2bd1
alejuran <alejandrourn@gmail.com>
2022-03-29 21:09:04 -0500

    first commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void printfunc(void) __attribute__((constructor));
+
+/**
+ * printfunc - unction that prints string before the main function is executed
+ */
+void printfunc(void)
+{
+	printf("You're beat! and yet; you must allow;\n");
+	printf("I bore my house upon my back!\n");
+}
