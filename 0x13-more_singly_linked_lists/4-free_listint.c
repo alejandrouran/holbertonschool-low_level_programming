1ccb2a5b57672cfeb17d9bcbea0853b2abfb859c
alejuran <alejandrourn@gmail.com>
2022-04-01 11:34:15 -0500

    free commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listing - frees a list
+ * @head: pointer
+ * return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *t, *c;
+
+	c = head;
+
+	while (c != NULL)
+	{
+		t = c->next;
+		free(c);
+		c = t;
+	}
+}
