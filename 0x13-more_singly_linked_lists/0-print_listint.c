0e7319105c2729a3ea7fb7432db959e8e9f79a2f
alejuran <alejandrourn@gmail.com>
2022-04-01 09:57:37 -0500

    listint commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: pointer
+ * Return: void
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
