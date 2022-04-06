10e66c9c890fd13ca6597a703e4d66ae1ca21fc1
alejuran <alejandrourn@gmail.com>
2022-04-06 18:35:19 -0500

    cp commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (argv[1] == NULL)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	fd_a = open(argv[1], O_RDONLY);
 	if (fd_a == -1)
@@ -43,9 +43,9 @@ int main(int argc, char *argv[])
 		if (w == -1 || w != r)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-		if (close(fd_b) == -1)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_a), exit(100);
-		if (close(fd_b) == -1)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_b), exit(100);
-		return (0);
+	if (close(fd_a) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_a), exit(100);
+	if (close(fd_b) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_b), exit(100);
+	return (0);
 }
