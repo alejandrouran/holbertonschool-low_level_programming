62e2d446ada83ec6f292d209aed67e9a7fe46c7c
alejuran <alejandrourn@gmail.com>
2022-04-28 14:53:31 -0500

    update triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,9 +10,13 @@ void print_triangle(int size)
 {
 	int x, y, z;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (x = 0; y < size; x++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x <= size; x++)
 		{
 			for (z = size - 1; z > x; z--)
 				_putchar(' ');
@@ -21,7 +25,6 @@ void print_triangle(int size)
 
 				_putchar('\n');
 		}
-	}
-	else
 		_putchar('\n');
+	}
 }
