47cf1f1c39915ed5cebde26836fdc685c8dc5409
alejandro <alejandrourn@gmail.com>
2022-05-12 15:45:00 -0500

    delete commit

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index
+ * @head: main node
+ * @index: node index
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *t = *head;
+	dlistint_t *tm;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(t);
+		return (1);
+	}
+	while (t)
+	{
+		if (index == i + 1)
+		{
+			tm = t->next;
+			t->next = tm->next;
+			free(tm);
+			return (1);
+		}
+		t = t->next;
+		i++;
+	}
+	return (-1);
+}
