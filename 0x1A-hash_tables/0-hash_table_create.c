e74481a325029df2184a8cbcde5185fce999d040
alejandro <alejandrourn@gmail.com>
2022-06-06 12:56:52 -0500

    hash update commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,11 +2,11 @@
 /**
  * hash_table_create - creates a has table
  * @size: insigned long int size
- * Return: has table
+ * Return: hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
